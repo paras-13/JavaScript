ff6206245b4356c97bed3dd1ec113156ea5735ae
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-01-05 03:25:37 +0530

    Variables in JavaScript

---
+++ 02_Variavles.js
@@ -0,0 +1,7 @@
+console.log("This repl contains code for the javascript course video 2");
+let a = 25;
+console.log(a);
+a = "Paras";
+console.log(a)
+
+// Variables in Javascript can contain $ and _
