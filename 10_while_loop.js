82e25cc52a6b8889fc60c6851e0d4a5f4948b31f
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-01-09 02:06:52 +0530

    While loop in JS

---
+++ 10_while_loop.js
@@ -0,0 +1,19 @@
+// while loop in JavaScript
+let n = prompt("Enter the value of n");
+n = Number.parseInt(n);
+/*
+let i = 0;
+while(i<n){
+  console.log(i);
+  i++;
+}
+console.log()
+*/
+
+// do- while loop
+let i = 10
+// in do-while loop firstly the do part executes i.e; atleast onetime output will come
+do {
+  console.log(i)
+  i++;
+} while (i < n)
