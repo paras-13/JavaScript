2fafb306939f04f61ac6bb773435a63712be326c
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-01-20 20:52:31 +0530

    Exercise 1

---
+++ exercise1.js
@@ -0,0 +1,34 @@
+// Chapter 1 {Practise Set}
+
+// Question 1
+let a = "Paras"
+let b = 4
+console.log(a + b)
+
+// Question 2
+let c = a + b
+console.log(typeof c)
+console.log(typeof b)
+
+//Question 3
+const i = {
+  "name": "Paras"
+}
+console.log(i)
+console.log(i["name"])
+
+//Question 4
+i["friend"] = "Utkarsh"        // We can add and update element in an object
+i["name"] = "Paras Upadhyay"
+console.log(i)
+
+// Question 5
+const dict = {
+  appreciate: "recognize the full worth of",
+  ataraxia: "state of freedom",
+  gnomon: "raised part of the sundail",
+  yaka: "hard work",
+  aver: "afferm with confidence"
+}
+console.log(dict.ataraxia)
+console.log(dict)
