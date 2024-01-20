6702f05ce71ca8532aa40c26fef14875417a0a71
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-01-20 20:49:46 +0530

    Declare Variable

---
+++ 02_variables.js
@@ -0,0 +1,8 @@
+console.log("This repl contains code for the javascript course video 2");
+let a = 25;
+console.log(a);
+a = "Paras";
+console.log(a)
+
+// Variables in Javascript can contain $ and _
+// Variables in JS can be declared automatically or by using let, var and const keywords
